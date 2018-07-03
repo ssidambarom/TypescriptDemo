bb0fe59690da774645977fa4a1bfbbf78f8d95ca
Sylvain-pro <sidambaroms@gmail.com>
2018-07-03 06:19:17 +0200

    reorganize project

--- arrayExample.ts
+++
@@ -1,5 +0,0 @@
-var numbers: number[] = [1, 2, 3.14];
-
-var names: string[] = ["Pierre","Paul","Jaques"];
-
-var otherNames: Array<string> = ["Pierre","Paul","Jaques"];
--- castingSynthaxe.ts
+++
@@ -1,6 +0,0 @@
-var myObject = <any>{id:1, name:"John"};
-
-myObject.id // id est du type any par le typage 
-            // par déduction du compilateur
-
-var myOtherObject = {id:2, name:"Peter"} as any;
