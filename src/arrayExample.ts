6f06f53de71485f213f245ab413fd0a001ea53db
Sylvain-pro <sidambaroms@gmail.com>
2018-07-03 06:20:06 +0200

    folders

---
+++ src/arrayExample.ts
@@ -0,0 +1,5 @@
+var numbers: number[] = [1, 2, 3.14];
+
+var names: string[] = ["Pierre","Paul","Jaques"];
+
+var otherNames: Array<string> = ["Pierre","Paul","Jaques"];
---
+++ src/file.ts
@@ -0,0 +1,11 @@
+
+var simple: string = 'string simple quote'
+var double: string = "string avec double quote";
+
+var version: number = 1;
+var template: string = `Mon super template version: ${version}
+
+Cool!`;
+
+var equivalentTemplate: string = 'Mon super template version: '+ version 
+                                + '\n\n' + 'Cool!';
